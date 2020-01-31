8358b3b6c2014b37cbb2fc705e2af584c6cead43
Justin Evans <evansjustindavid@gmail.com>
2020-01-31 19:45:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var kittens = ['Milo', 'Otis', 'Garfield']
 
 // Add your functions and code here
 
-function destructivelyAppendKitten(a, b) {
-  return a.push(b)  
+function destructivelyAppendKitten(b) {
+  return kittens.push(b)  
 }
